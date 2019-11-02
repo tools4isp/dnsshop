16ebbb55e9a641460b0a39fb82b5fd1368a997d3
nlmark <github@mscholten.eu>
2019-11-02 14:56:18 +0100

    Without this change the field might not be long enough for dnskey values

--- powerdns.sql
+++ powerdns.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `dnssec` (
   `id` bigint(255) NOT NULL auto_increment,
   `domainid` int(255) NOT NULL,
   `type` varchar(10) default NULL,
-  `record` varchar(255) NOT NULL,
+  `record` text NOT NULL,
  PRIMARY KEY (`id`),
  KEY `domainid` (`domainid`,`type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
